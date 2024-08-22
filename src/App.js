bbf99bd105d3188a503b4581b42d357290614a95
nihal verma <nihalv197@gmail.com>
2024-08-22 14:05:10 +0530

    Assignment

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import GetMethod from "./GetMethod";
+function App() {
+  return (
+    <div className="App">
+      <h1>Api Handling Project</h1>
+
+      {/* Example 1 */}
+      <GetMethod />
+    </div>
+  );
+}
+export default App;
